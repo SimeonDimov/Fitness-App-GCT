ffe0c911bab1e1982d792819a05f41669b4ae88f
Simeon Dimov <s.dimov@student.fontys.nl>
2022-09-15 17:10:31 +0200

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Simeon Dimov on 15/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world 123")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Fitness_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_AppApp.swift
+//  Shared
+//
+//  Created by Simeon Dimov on 15/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
